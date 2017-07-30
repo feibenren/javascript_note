72ad2271017c4446314af8016f50fdb183265d21
feibenren <1057808091@qq.com>
2017-07-30 22:29:29 +0800

    xxx

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
-var name = "The Window";　　
-var object = {　　　　 name: "My Object", 　　　　getNameFunc: function() {　　　　　　 return function() {　　　　　　　　 return this.name;　　　　　　 };　　　　 }　　 };　　
-console.log(object.getNameFunc()());
+function Person(name) {
+    this.name = name;
+    this.sex = 'nan';
+}
+Person.prototype.school = 'jinshan';
+var p1 = new Person('wangbing');
+console.log(p1.valueOf());
